378e3ad34528325bf3acd354593281b636954122
vashisht arora <vashishtarora2008@gmail.com>
2024-07-31 01:00:36 +0530

    new

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -2,6 +2,7 @@ import os
 
 def main():
   print("Hello from GitHub Actions!")
+  print(f'::set-output name=bbb::567')
   token = os.environ.get("AZURE_SECRET_TOKEN")
   if not token:
     raise RuntimeError("AZURE_SECRET_TOKEN env var is not set!")

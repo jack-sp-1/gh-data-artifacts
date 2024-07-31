7467f6e2ae7d5c5b62429b37d5d6692da73b7723
vashisht arora <vashishtarora2008@gmail.com>
2024-07-31 14:56:05 +0530

    new

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -8,6 +8,16 @@ def main():
     raise RuntimeError("AZURE_SECRET_TOKEN env var is not set!")
   print("All good! we found our env var")
   
+  with open('.github/workflows/params.json') as f:
+    data = json.load(f)
+    istl_in_e1 = data[0]['install_e1']
+    istl_in_e2 = data[0]['install_e2']
+    istl_in_e3 = data[0]['install_e3']
+
+   f.close() 
+  print(f'::set-output name=istl_in_e1::{istl_in_e1}')
+
+
 
 if __name__ == '__main__':
   main()

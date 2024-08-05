dc7dd9529b2e945f0c2b5b93aaa590c38172595b
vashisht arora <vashishtarora2008@gmail.com>
2024-08-05 16:27:05 +0530

    new

---
+++ .github/actions/local-one/check_ls.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import sys
+
+if __name__=='__main__':
+    main()
+
+def main():
+    parser = argparse.ArgumentParser(
+                    prog='llist',
+                    description='to list files/dir',
+                    epilog='Text at the bottom of help')    
+
+    parser.add_argument('--root-path', type=str, default=os.getcwd(),help='for root')
+    parser.add_argument('--dir-pattern', type=str, default='',help='for root')
+    parser.add_argument('--delim', type=str, default=',',help='for root')
+    parser.add_argument('--file_exlude_regex', type=str, default='',help='for root')
+    parser.add_argument('--dir_exclude_regex', type=str, default='',help='for root')
+    parser.add_argument('--root-path', type=str, default=os.getcwd(),help='for root')
+
+args = parser.parse_args()
+print(f"args are:{args}: the end>>>>")
+print(args.accumulate(args.integers))
+
